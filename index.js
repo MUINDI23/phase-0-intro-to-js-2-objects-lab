8c323bf5a000a1391e7cd6ffdb5e7f79a99e6e1b
MUINDI23 <bbamnrb203915@spu.ac.ke>
2025-03-19 22:07:54 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+//Declairing object
+const employee = {
+    name: "James",
+    streetAddress: "113 Kabiria",
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee.name = "Sam";
+    employee.streetAddress = "12 Broadway";
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee.name;
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
